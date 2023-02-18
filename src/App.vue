5c77785fc363ffe1c67e4ffd3ecf520503e741a3
nilufarxon <orifjonova07@inbox.ru>
2023-02-18 20:41:27 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<!-- <template>
+  <div class="container">
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" type="text" />
+    <label for="email">Email:</label>
+    <input id="email" v-model="email" type="email" />
+    <label for="password">Password:</label>
+    <input id="password" v-model="password" type="password" />
+    <label for="remember-me">Remember me:</label>
+    <input id="remember-e" v-model="rememberMe" type="checkbox" />
+    <label for="name">Favourite color</label>
+    <select v-mode="FavoriteColor">
+      <option value="red">red</option>
+      <option value="green">green</option>
+      <option value="yellow">yellow</option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      rememberMe: false,
+      FavoriteColor: "red",
+    };
+  },
+};
+</script>
+<style></style> -->
+<template>
+  <div>
+    <div class="nav-bar"></div>
+  </div>
+</template>
